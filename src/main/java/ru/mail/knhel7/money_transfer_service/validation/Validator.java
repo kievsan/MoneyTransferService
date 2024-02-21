7e3b0071e430649f265f1e236ac6f48c37a3650c
kievskiy <knhel7@mail.ru>
2024-02-21 20:17:54 +0300

    corrections to the list of comments

--- src/main/java/ru/mail/knhel7/money_transfer_service/validation/Validator.java
+++ src/main/java/ru/mail/knhel7/money_transfer_service/validation/Validator.java
@@ -16,7 +16,7 @@ public class Validator {
     }
 
     public Transaction<Transfer> validateTransferConfirm(TransferConfirm confirm) {
-        String err = "Перевод №" + confirm.getOperationId() + " не подтвержден: не найден...";
+        String err = "Перевод №" + confirm.getOperationId() + " не подтвержден";
         if (isValidTransferConfirm(confirm)) {
             throw new NotFoundEx(err);
         }
@@ -24,7 +24,7 @@ public class Validator {
             int ID = Integer.parseInt(confirm.getOperationId());
             return (Transaction<Transfer>) transactionsRepo.getTransactionByID(ID).get();
         } catch (NumberFormatException NoSuchElementException) {
-            throw new NotFoundEx(err);
+            throw new NotFoundEx(err + ": не найден...");
         }
     }
 
@@ -39,9 +39,30 @@ public class Validator {
     }
 
     public void validateTransferMoney(Transfer transfer) {
+        if (transfer == null) {
+            throw new TransferException("Пустой перевод не предусмотрен!");
+        }
         if (transfer.getAmount().value() <= 0) {
             throw new TransferException("Перевод отрицательной/нулевой суммы не предусмотрен!");
         }
+        if (transfer.getCardFromNumber() == null || transfer.getCardFromNumber().length() != 16) {
+            throw new TransferException("Ошибка в номере карты отправителя!");
+        }
+        if (transfer.getCardToNumber() == null || transfer.getCardToNumber().length() != 16) {
+            throw new TransferException("Ошибка в номере карты получателя!");
+        }
+        if (transfer.getCardFromCVV() == null || transfer.getCardFromCVV().length() != 3) {
+            throw new TransferException("Ошибка в коде CVV карты отправителя: должно быть 3 цифры!");
+        }
+        if (transfer.getCardFromValidTill() == null || transfer.getCardFromValidTill().length() != 5) {
+            throw new TransferException("Ошибка в дате: должно быть 5 знаков!");
+        }
+        if (isOverdueDate(transfer.getCardFromValidTill())) {
+            throw new TransferException("Карта отправителя просрочена!");
+        }
+    }
 
+    private boolean isOverdueDate(String d) {
+        return false;
     }
 }
